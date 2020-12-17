3b59484268cf4d4efa15ecc4104c0606dcdf27f0
Amardeep Yadav <amaryadav344@gmail.com>
2020-12-17 16:40:14 +0530

    Removed unused properties

--- app/src/main/java/com/loopwiki/movieground/Model/Movie.java
+++ app/src/main/java/com/loopwiki/movieground/Model/Movie.java
@@ -3,81 +3,19 @@ package com.loopwiki.movieground.Model;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
-import java.sql.Date;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.List;
-
 /*
-    POJO class to contain movie json details
+    POJO class to contain movie JSON details
  */
 public class Movie {
-    @SerializedName("adult")
-    @Expose
-    private Boolean adult;
-    @SerializedName("backdrop_path")
-    @Expose
-    private String backdropPath;
-    @SerializedName("genre_ids")
-    @Expose
-    private List<Integer> genreIds = null;
     @SerializedName("id")
     @Expose
     private Integer id;
-    @SerializedName("original_language")
-    @Expose
-    private String originalLanguage;
-    @SerializedName("original_title")
-    @Expose
-    private String originalTitle;
-    @SerializedName("overview")
-    @Expose
-    private String overview;
-    @SerializedName("popularity")
-    @Expose
-    private Double popularity;
     @SerializedName("poster_path")
     @Expose
     private String posterPath;
-    @SerializedName("release_date")
-    @Expose
-    private String releaseDate;
-    @SerializedName("title")
-    @Expose
-    private String title;
-    @SerializedName("video")
-    @Expose
-    private Boolean video;
     @SerializedName("vote_average")
     @Expose
     private Double voteAverage;
-    @SerializedName("vote_count")
-    @Expose
-    private Integer voteCount;
-
-    public Boolean getAdult() {
-        return adult;
-    }
-
-    public void setAdult(Boolean adult) {
-        this.adult = adult;
-    }
-
-    public String getBackdropPath() {
-        return backdropPath;
-    }
-
-    public void setBackdropPath(String backdropPath) {
-        this.backdropPath = backdropPath;
-    }
-
-    public List<Integer> getGenreIds() {
-        return genreIds;
-    }
-
-    public void setGenreIds(List<Integer> genreIds) {
-        this.genreIds = genreIds;
-    }
 
     public Integer getId() {
         return id;
@@ -85,38 +23,6 @@ public class Movie {
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    public String getOriginalLanguage() {
-        return originalLanguage;
-    }
-
-    public void setOriginalLanguage(String originalLanguage) {
-        this.originalLanguage = originalLanguage;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public void setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
-    }
-
-    public String getOverview() {
-        return overview;
-    }
-
-    public void setOverview(String overview) {
-        this.overview = overview;
-    }
-
-    public Double getPopularity() {
-        return popularity;
-    }
-
-    public void setPopularity(Double popularity) {
-        this.popularity = popularity;
     }
 
     public String getPosterPath() {
@@ -127,46 +33,12 @@ public class Movie {
         this.posterPath = posterPath;
     }
 
-    public String getReleaseDate() {
-        Calendar calendar = new GregorianCalendar();
-        calendar.setTime(Date.valueOf(releaseDate));
-        return String.valueOf(calendar.get(Calendar.YEAR));
-    }
-
-    public void setReleaseDate(String releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Boolean getVideo() {
-        return video;
-    }
-
-    public void setVideo(Boolean video) {
-        this.video = video;
-    }
-
     public Double getVoteAverage() {
         return voteAverage;
     }
 
     public void setVoteAverage(Double voteAverage) {
         this.voteAverage = voteAverage;
-    }
-
-    public Integer getVoteCount() {
-        return voteCount;
-    }
-
-    public void setVoteCount(Integer voteCount) {
-        this.voteCount = voteCount;
     }
 
     @Override
--- app/src/main/java/com/loopwiki/movieground/Model/MovieResponse.java
+++ app/src/main/java/com/loopwiki/movieground/Model/MovieResponse.java
@@ -4,6 +4,7 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
+
 /*
     POJO class for to contain movie response json details
  */
